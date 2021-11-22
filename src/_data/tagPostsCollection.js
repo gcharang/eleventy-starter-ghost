3fa9a70a7fca676cebb8355bdd2e714238e0c3c4
Jashwanth <jashwanth.work@gmail.com>
2021-11-22 17:57:00 +0530

    added listing posts by tag

---
+++ src/_data/tagPostsCollection.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const ghostContentAPI = require("@tryghost/content-api");
+const api = new ghostContentAPI({
+    url: process.env.GHOST_API_URL,
+    key: process.env.GHOST_CONTENT_API_KEY,
+    version: "v2",
+});
+
+const stripDomain = (url) => {
+    return url.replace(process.env.GHOST_API_URL, "");
+};
+
+//Create a tag post collection
+module.exports = async function() {
+    const allTags = await api.tags
+        .browse({
+            include: "count.posts",
+            limit: "all",
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    const allPosts = await api.posts
+        .browse({
+            include: "tags,authors",
+            limit: "all",
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+ 
+    let tagPostsCollection = [];
+    allTags.forEach(tag => {
+        const taggedPosts = allPosts.filter(post => {
+            post.url = stripDomain(post.url);
+            const postTagNames = post.tags.map(postTag=>postTag.name)
+            return postTagNames.includes(tag.name);
+        });
+
+        const postsPerPage = 7;
+        for(let i = 0; i*postsPerPage < taggedPosts.length; i++){
+            let postSet = {};
+            postSet.posts = taggedPosts.slice( postsPerPage*(i) , postsPerPage*(i+1));
+            postSet.url = (i > 0) ? tag.slug + `/${i}/` : tag.slug + "/";
+            tagPostsCollection.push(postSet);
+        }
+    })
+
+    return tagPostsCollection;
+}
